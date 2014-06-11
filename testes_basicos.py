530c422ac0fd482e491db46cdf250145e0c22f60
renzon <renzon@gmail.com>
2014-06-11 10:49:31 -0300

    Alterações para teste funcionarem via linha de comando

--- testes_basicos.py
+++ testes_basicos.py
@@ -2,12 +2,16 @@ import unittest
 
 
 def soma(param, param1):
-    return param+param1
+    return param + param1
 
 
 class BasicoTests(unittest.TestCase):
-    def soma_test(self):
-        resultado=soma(1,2)
-        self.assertEqual(3,resultado)
-        resultado=soma(3,2)
-        self.assertEqual(5,resultado)
+    def test_soma(self):
+        resultado = soma(1, 2)
+        self.assertEqual(3, resultado)
+        resultado = soma(3, 2)
+        self.assertEqual(5, resultado)
+
+
+if __name__ == '__main__':
+    unittest.main()
